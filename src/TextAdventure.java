60ab74b48b1dd6a949a30e4ba136bd07cbbe2a18
Trevor <trevor.pirone1@marist.edu>
2016-01-30 18:40:30 -0500

    Added a welcome message to the text adventure game. Also imported Scanner and Random package.

---
+++ src/TextAdventure.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class TextAdventure {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Text Adventure Game!");
+		System.out.println("-----------------------------------");
+
+	}
+
+}
