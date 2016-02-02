3de67e63f21e7cb2245bf24a5524e844962855e0
Trevor <trevor.pirone1@marist.edu>
2016-02-02 18:46:35 -0500

    If the user types in a capital H, it will display a help message that allows the user to figure out how to play the game.

--- src/TextAdventure.java
+++ src/TextAdventure.java
@@ -16,18 +16,31 @@ public class TextAdventure {
 		Scanner inputSource = new Scanner(System.in);
 		String input;
 		String direction;
+		
 		//prints a greeting message when the user runs the program
 		System.out.println("Welcome to the Text Adventure Game!");
 		System.out.println("-----------------------------------");
 		System.out.print("What is your name? ");
 		input = inputSource.nextLine();
+		
 		//the user will type their name here
 		System.out.println("Hello, " + input + "!");
 		System.out.println("\n");//prints out a new line of whitespace
 		System.out.println("Press the H key on the keyboard before playing the game to read the instructions.");
-		System.out.println("If you know the instructions already, press the Enter key on the keyboard.");
-		System.out.print("If by mistake you opened this application, press Q on the keyboard to quit the game. ");
+		System.out.println("If by mistake you opened this application, press Q on the keyboard to quit the game.");
 		//the user will decide to start, get help or exit the game here
+		
+		while(true) {
+			System.out.print("\nEnter a command: ");
+			input = inputSource.nextLine();
+			
+			if (input.equals("H")) {
+				System.out.println("Press N to go North.");
+				System.out.println("Press S to go South.");
+				System.out.println("Press E to go East.");
+				System.out.println("Press W to go West.");
+			}
+		}
 		
 
 	}
