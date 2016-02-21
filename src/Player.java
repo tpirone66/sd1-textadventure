edc412d20cabc03a959978c0a5f846b77ac89a5c
Trevor <trevor.pirone1@marist.edu>
2016-02-21 14:34:46 -0500

    Added some comments into the Player.class file.

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,16 @@
+//Player class for Marist Mayhem
+import java.util.Scanner;
 
 public class Player {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner inputSource = new Scanner(System.in);
+		//Some of the player variables that will be in this class
+		String name;
+		String location;
+		int inventory;
+		int score;
+		
 
 	}
 
