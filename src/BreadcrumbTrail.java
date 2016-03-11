50652e51266b6b4e8d8982676814d4e3952fb215
Trevor <trevor.pirone1@marist.edu>
2016-03-10 19:20:43 -0500

    Created the methods in the BreadcrumbTrail class.

--- src/BreadcrumbTrail.java
+++ src/BreadcrumbTrail.java
@@ -1,3 +1,27 @@
-public class BreadcrumbTrail {
+import java.util.ArrayList;
 
+public class BreadcrumbTrail {
+	
+	private Object[] pickupCrumb;
+	private int crumb;
+	
+	public void dropCrumb() {
+		 if (this.hasMoreCrumb() == false) {
+	            crumb--;
+		 }
+	}
+	
+	public void pickupCrumb(Object x) {
+		
+	}
+	
+	public Object currentCrumb() {
+		return pickupCrumb[crumb];
+		
+	}
+	
+	public boolean hasMoreCrumb() {
+		return false;
+		
+	}
 }
